db5c449402d4125e0e13a144078f4dbedab49d80
Pakgon <Pakgon@Pakgons-iMac.local>
2017-04-18 11:13:33 +0700

    Initial Commit

---
+++ payment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  payment
+//
+//  Created by Pakgon on 4/18/2560 BE.
+//  Copyright © 2560 Pakgon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
